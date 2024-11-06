8000c37c3aac8e6983e3f86f9e5c04edb544f2f0
Emil Haraldsø <emiljohansenharaldsoe@outlook.com>
2024-11-06 11:53:16 +0100

    added a way to let user know they have an available offer

--- barnehage/kg.py
+++ barnehage/kg.py
@@ -24,17 +24,29 @@ def barnehager():
 def behandle():
     if request.method == 'POST':
         sd = request.form
-        print(sd)
-        log = insert_soknad(form_to_object_soknad(sd))
-        print(log)
-        session['information'] = sd
-        return redirect(url_for('svar')) #[1]
+        application = form_to_object_soknad(sd)
+        
+        # Determine offer/rejection based on criteria (e.g., available spots, income, priority)
+        selected_barnehage = next((b for b in select_alle_barnehager() if b.barnehage_id == int(sd['barnehage_id'])), None)
+        
+        if selected_barnehage and selected_barnehage.barnehage_ledige_plasser > 0:
+            # Here you could add more criteria to decide on "Tilbud" or "Avslag"
+            status = 'Tilbud'
+        else:
+            status = 'Avslag'
+
+        # Insert application into the database and save in session
+        insert_soknad(application)
+        session['information'] = {**sd, 'status': status}  # Add the status to session data
+
+        return redirect(url_for('svar'))
     else:
         return render_template('soknad.html')
 
+
 @app.route('/svar')
 def svar():
-    information = session['information']
+    information = session.get['information']
     return render_template('svar.html', data=information)
 
 @app.route('/commit')
